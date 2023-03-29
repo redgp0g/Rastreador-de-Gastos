baee07a9fbccc19f0d665534f61fa81604817807
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-03-28 23:37:56 -0300

    Dashboard finalizado - Configurações de SpineChart do Dashboard finalizada - Inicio da criação das transações recentes

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -50,9 +50,64 @@ namespace Rastreador_de_Gastos.Controllers
 					amount = k.Sum(j => j.Amount),
 					formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
 				})
+				.OrderByDescending(l=>l.amount)
 				.ToList();
 
-            return View();
+			//Spline Chart - Income vs Expense
+			//Renda
+			List<SplineChartData> IncomeSummary = SelectedTransactions
+				.Where(i => i.Category.Type == "Renda")
+				.GroupBy(j => j.Date)
+				.Select(k => new SplineChartData()
+				{
+					day = k.First().Date.ToString("dd-MMM"),
+					income = k.Sum(l => l.Amount)
+				})
+				.ToList();
+
+			//Despesa
+			List<SplineChartData> ExpenseSummary = SelectedTransactions
+				.Where(i => i.Category.Type == "Despesa")
+				.GroupBy(j => j.Date)
+				.Select(k => new SplineChartData()
+				{
+					day = k.First().Date.ToString("dd-MMM"),
+					expense = k.Sum(l => l.Amount)
+				})
+				.ToList();
+
+			//Combine Income & Expense
+			string[] Last7Days = Enumerable.Range(0,7)
+				.Select(i => StartDate.AddDays(i).ToString("dd-MMM"))
+				.ToArray();
+
+			ViewBag.SplineChartData = from day in Last7Days
+									  join income in IncomeSummary on day equals income.day into dayIncomeJoined
+									  from income in dayIncomeJoined.DefaultIfEmpty()
+									  join expense in ExpenseSummary on day equals expense.day into expenseJoined
+									  from expense in expenseJoined.DefaultIfEmpty()
+									  select new
+									  {
+										  day = day,
+										  income = income == null ? 0 : income.income,
+										  expense = expense == null ? 0 : expense.expense,
+									  };
+
+			//Transações Recentes
+			ViewBag.RecentTransactions = await _context.Transaction
+				.Include(i => i.Category)
+				.OrderByDescending(j => j.Date)
+				.Take(5)
+				.ToListAsync();
+
+			return View();
 		}
+	}
+
+	public class SplineChartData
+	{
+		public string day;
+		public int income;
+		public int expense;
 	}
 }
