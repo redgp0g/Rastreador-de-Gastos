d779585c29f3cdbefce26fc96c6506242e211eab
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-03-28 21:28:55 -0300

    Transaction CRUD - AddOrEdit Completo - Criação de atributos na Transaction para quantidade formatada e titulo da categoria com Ícone - Index com editar, visualizar e excluir funcionando - Remoção de View Edit e Delete - Atualização do CodeGeneration.Design para 7.0.5

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -8,10 +8,27 @@ namespace Rastreador_de_Gastos.Models
 		[Key] 
 		public int TransactionId { get; set; }
 		public int CategoryId { get; set; }
-		public Category Category { get; set; }
+		public Category? Category { get; set; }
 		public int Amount { get; set; }
 		[Column(TypeName = "nvarchar(75)")]
 		public string? Note { get; set; }
 		public DateTime Date { get; set; } = DateTime.Now;
+
+		[NotMapped]
+		public string? CategoryTitleWithIcon { 
+			get 
+			{
+				return Category == null ? "" : Category.Icon + " " + Category.Title;
+			} 
+		}
+
+		[NotMapped]
+		public string? FormattedAmount
+		{
+			get
+			{
+				return ((Category == null || Category.Type=="Despesa" )? "- " : "+ ") + Amount.ToString("C0");
+			}
+		}
 	}
 }
